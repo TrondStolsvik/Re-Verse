4807c9c465a16b4bb637b674f9af021945471847
TrondStolsvik <trondstolsvik@gmail.com>
2022-09-14 18:34:42 +0200

    bug: not reversing
    trying to figure out reversing the string.
    code _seems_ right

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
-function HelloWorld() {
-  alert("Hello World!");
+//Get the string from the input on the page
+function getString() {
+  //First clear the content of the page
+  document.getElementById("results").innerHTML = "";
+
+  document.getElementById("results").classList.add("invisible");
+
+  //Declare a variable to hold the string
+  let stringInput = document.getElementById("stringInput").value;
+
+  let reversedString = revString(stringInput);
+
+  displayString(reversedString);
+}
+
+//Reverse the string
+function revString(stringInput) {
+  //Declare an empty variable as a string
+  let reversedString = [];
+
+  //For-loop through the string from the end
+  for (let i = stringInput.length - 1; i >= 0; i--) {
+    //Concatenate the result of each iteration to the empty array
+    reversedString += stringInput[i];
+  }
+  return reversedString;
+}
+
+//Display the reversed string on the page
+function revString(reversedString) {
+  document.getElementById("results").classList.remove("invisible");
+
+  document.getElementById(
+    "results"
+  ).innerHTML = `Your string reversed is: ${reversedString}`;
 }
