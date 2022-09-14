6275943ca0294101a3b3d6c6bf793883acf1a1d5
TrondStolsvik <trondstolsvik@gmail.com>
2022-09-14 19:08:11 +0200

    code page done
    Explanations and comments added.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,16 @@
 function getString() {
   //First clear the content of the page
   document.getElementById("results").innerHTML = "";
-
+  //Add the incisible class to results so that it gets hidden at the start
   document.getElementById("results").classList.add("invisible");
 
   //Declare a variable to hold the string
   let stringInput = document.getElementById("stringInput").value;
 
+  //Declare a variable to hold the reversed string
   let reversedString = revString(stringInput);
 
+  //Call displayString to display the string
   displayString(reversedString);
 }
 
@@ -23,13 +25,15 @@ function revString(stringInput) {
     //Concatenate the result of each iteration to the empty array
     reversedString += stringInput[i];
   }
+  //Return the string for display
   return reversedString;
 }
 
 //Display the reversed string on the page
 function displayString(reversedString) {
+  //First remove the invisible class from results to show the results
   document.getElementById("results").classList.remove("invisible");
-
+  //Inject the results on the page
   document.getElementById(
     "results"
   ).innerHTML = `Your string reversed is: ${reversedString}`;
